ec2be874bcdfa7e65e238dc47192213cb6d9587c
waitthisisnt2015 <waitthisisnt2015@noreply.codeberg.org>
2023-10-24 15:53:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
-function textToUwu(inputText, pluginManifest) {
-    if (pluginManifest && pluginManifest.entrypoint) {
-        // Use the provided plugin's entrypoint to transform the text
-        const transformFunction = new Function('text', pluginManifest.entrypoint);
-        const transformedText = transformFunction(inputText);
-        return transformedText;
-    } else {
-        // Fallback to the default transformation
-        let text = inputText;
-        text = text.replace(/(?:r|l)/g, 'w');
-        text = text.replace(/(?:R|L)/g, 'W');
-        text = text.replace(/th/g, 'f');
-        text = text.replace(/TH/g, 'F');
-        text = text.replace(/you/g, 'u');
-        text = text.replace(/You/g, 'U');
-        text = text.replace(/ove/g, 'uv');
-        text = text.replace(/s/g, 'sh');
-        text = text.replace(/S/g, 'Sh');
-        text = text.replace(/\./g, ' uwu.');
-        text = text.replace(/!/g, ' uwu!');
-        text = text.replace(/\?/g, ' uwu?');
-        return text;
-    }
+if (typeof uwuifyPlugin === "undefined") {
+    const uwuifyPlugin = {
+        onMessage: (message) => {
+            if (message.content) {
+                message.content = message.content
+                    .replace(/(?:R|L)/g, 'W')
+                    .replace(/th/g, 'f')
+                    .replace(/TH/g, 'F')
+                    .replace(/you/g, 'u')
+                    .replace(/You/g, 'U')
+                    .replace(/ove/g, 'uv')
+                    .replace(/s/g, 'sh')
+                    .replace(/S/g, 'Sh')
+                    .replace(/\./g, ' uwu.')
+                    .replace(/!/g, ' uwu!')
+                    .replace(/\?/g, ' uwu?');
+            }
+            return message;
+        }
+    };
+
+    window.revoltPlugins = window.revoltPlugins || [];
+    window.revoltPlugins.push(uwuifyPlugin);
+    console.log('uwuifyPlugin has been loaded.');
+} else {
+    console.log('uwuifyPlugin is already loaded. Avoiding redeclaration.');
 }
-
-// Example usage with a plugin:
-const inputText = "Hello there, how are you doing?";
-const plugin = {
-    entrypoint: `(text) => {
-        return text.replace(/are/g, 'r');
-    }`
-};
-
-const uwuText = textToUwu(inputText, plugin);
-console.log(uwuText);
